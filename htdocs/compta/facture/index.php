4886f6ce63527d591114a5ace12be9dc644b3383
Laurent Destailleur <eldy@destailleur.fr>
2024-11-04 21:28:59 +0100

    Debug v21

--- htdocs/compta/facture/index.php
+++ htdocs/compta/facture/index.php
@@ -43,8 +43,8 @@ if (isset($user->socid) && $user->socid > 0) {
 
 // Maximum elements of the tables
 $max = getDolGlobalInt('MAIN_SIZE_SHORTLIST_LIMIT', 5);
-$maxDraftCount = !getDolGlobalString('MAIN_MAXLIST_OVERLOAD') ? $max : $conf->global->MAIN_MAXLIST_OVERLOAD;
-$maxOpenCount = !getDolGlobalString('MAIN_MAXLIST_OVERLOAD') ? $max : $conf->global->MAIN_MAXLIST_OVERLOAD;
+$maxDraftCount = getDolGlobalInt('MAIN_MAXLIST_OVERLOAD', $max);
+$maxOpenCount = getDolGlobalInt('MAIN_MAXLIST_OVERLOAD', $max);
 
 
 /*
